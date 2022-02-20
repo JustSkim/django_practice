911e6399a1661652eff5569bc9c137aaaaa74815
JustSkim <1424266960@qq.com>
2022-02-20 21:52:37 +0800

    过滤条件

--- sales/views.py
+++ sales/views.py
@@ -10,6 +10,9 @@ def listorders_sales(request):
 
 # 引入Customer对象定义
 from common.models import Customer
+
+
+
 def listcustomers(request):
     #返回一个QuerySet对象，包含所有的表纪录
     #每条表纪录都是是一个dict对象
@@ -19,6 +22,19 @@ def listcustomers(request):
 objects表示基类（也就是其父类）。objects是Django帮我们自动生成的管理器对象，
 通过这个管理器可以实现对数据的查询， objects是models.Manager类的一个对象，如果我们模型类中添加一个models.Manger类或者其子类变量，那么Django就不再帮我们生成默认的objects管理器。
     '''
+
+    # 可以通过 filter 方法加入过滤条件，修改view里面的代码
+    # 检查url中是否有参数phoneNumber
+    ph =  request.GET.get('phoneNumber',None)
+
+    # 如果有，添加过滤条件
+    if ph:
+        qs = qs.filter(phoneNumber=ph)
+    '''
+    输入localhost:4000/sales/customers/?phoneNumber=1597，数据库中无符合的phoneNumber字段，不返回
+    输入localhost:4000/sales/customers/?phoneNumber=159753，返回数据库中符合的字段
+    '''
+
 
     #定义返回字符串
     retStr = ''
