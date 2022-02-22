d77c29be5546e069981301b6f4652d4bcaa3411b
JustSkim <1424266960@qq.com>
2022-02-22 22:14:05 +0800

    实现登录

--- mgr/urls.py
+++ mgr/urls.py
@@ -3,7 +3,9 @@
 结合bysms中的urls.py的路由路径，就表示凡是API 请求url为 /api/mgr/customers 的，都交由 我们上面定义的dispatch函数进行分派处理
 '''
 from django.urls import path
-from mgr import customer
+from mgr import customer,sign_in_out
 urlpatterns = [
     path('customers', customer.dispatcher),
+    path('signin', sign_in_out.signin),
+    path('signout', sign_in_out.signout),
 ]
